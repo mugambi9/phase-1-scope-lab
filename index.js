63e1f1f01c4f9e289d6ab0c82fdf05c7babc85db
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-07 21:52:04 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+
+
+var customerName = "bob"
+
+function upperCaseCustomerName (){
+    window.customerName = window.customerName.toUpperCase()
+} 
+function setBestCustomer(){
+    window.bestCustomer = "not bob"
+
+}
+function overwriteBestCustomer(){
+    window.bestCustomer = "maybe bob"
+}
+const leastFavoriteCustomer = "harrison"
+function changeLeastFavoriteCustomer (){
+
+    leastFavoriteCustomer = "harrison "
+}
+
+
+
+
+
